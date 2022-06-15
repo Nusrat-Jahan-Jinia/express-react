30af36de244ac9ab29f6f057ebfb7fa9faded617
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2022-06-15 23:26:35 +0600

    User created

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,10 +1,33 @@
 const express = require("express");
-const { getUsers } = require("../controller/usersController");
+const { check } = require("express-validator")
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
 
 const router = express.Router();
 
-// login page
+// users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+
+// add user
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+
+// remove user
+router.delete("/:id", removeUser);
 
 module.exports = router;
