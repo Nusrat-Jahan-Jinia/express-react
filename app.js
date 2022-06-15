ce881a7e06bfff85567f1d7722867fb301cca592
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-06-15 15:10:46 +0600

    Project final set up completed

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 const loginRouter = require("./router/loginRouter");
 const usersRouter = require("./router/usersRouter");
 const inboxRouter = require("./router/inboxRouter");
-PORT = 5000;
 
 const app = express();
+dotenv.config();
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch(err => console.log(err));
 
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
 // set view engine
 app.set("view engine", "ejs");
 
@@ -18,6 +41,11 @@ app.get("/", loginRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
 
-app.listen(PORT, () => {
-  console.log(`app listening to port ${PORT}`);
+// 404 not found handler
+app.use(notFoundHandler);
+
+// common error handler
+app.use(errorHandler);
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
 });
